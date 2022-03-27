dd2c6f659224bedc3336a1af3ff34692edb93095
Jacky Ma <303536@qq.com>
2022-03-27 15:22:18 +0800

    Nav and routes added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,57 @@
 <template>
-  <ManageMeal />
+<header>
+  <div class="navbar navbar-dark bg-dark shadow-sm">
+    <div class="container d-flex justify-content-between">
+      <a href="#" class="navbar-brand d-flex align-items-center">
+    <svg data-v-41be6633="" viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="egg" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-egg mx-auto b-icon bi"><g data-v-41be6633=""><path d="M8 15a5 5 0 0 1-5-5c0-1.956.69-4.286 1.742-6.12.524-.913 1.112-1.658 1.704-2.164C7.044 1.206 7.572 1 8 1c.428 0 .956.206 1.554.716.592.506 1.18 1.251 1.704 2.164C12.31 5.714 13 8.044 13 10a5 5 0 0 1-5 5zm0 1a6 6 0 0 0 6-6c0-4.314-3-10-6-10S2 5.686 2 10a6 6 0 0 0 6 6z"></path></g></svg>
+        <strong>今天吃啥</strong>
+    <svg data-v-41be6633="" viewBox="0 0 16 16" width="1em" height="1em" focusable="false" role="img" aria-label="tree" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi-tree mx-auto b-icon bi"><g data-v-41be6633=""><path d="M8.416.223a.5.5 0 0 0-.832 0l-3 4.5A.5.5 0 0 0 5 5.5h.098L3.076 8.735A.5.5 0 0 0 3.5 9.5h.191l-1.638 3.276a.5.5 0 0 0 .447.724H7V16h2v-2.5h4.5a.5.5 0 0 0 .447-.724L12.31 9.5h.191a.5.5 0 0 0 .424-.765L10.902 5.5H11a.5.5 0 0 0 .416-.777l-3-4.5zM6.437 4.758A.5.5 0 0 0 6 4.5h-.066L8 1.401 10.066 4.5H10a.5.5 0 0 0-.424.765L11.598 8.5H11.5a.5.5 0 0 0-.447.724L12.69 12.5H3.309l1.638-3.276A.5.5 0 0 0 4.5 8.5h-.098l2.022-3.235a.5.5 0 0 0 .013-.507z"></path></g></svg>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </div>
+</header>
+<nav class="navbar navbar-expand-sm bg-light navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item m-1">
+        <a class="btn btn-light btn-outline-primary"
+        href="#/">Home</a>
+    </li>
+    <li class="nav-item m-1">
+        <a class="btn btn-light btn-outline-primary"
+        href="#/ManageMeal">ManageMeal</a>
+    </li>
+    </ul>
+</nav>
+ <component :is="currentView" />
 </template>
 
 <script>
+import TodaysMeal from './components/TodaysMeal.vue'
 import ManageMeal from './components/ManageMeal.vue'
+import NotFound from './components/NotFound.vue'
+const routes = {
+  '/': TodaysMeal,
+  '/ManageMeal': ManageMeal
+}
+
 export default {
-  name: 'App',
-  components: {
-    ManageMeal,
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      return routes[this.currentPath.slice(1) || '/'] || NotFound
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+      this.currentPath = window.location.hash
+		})
   }
 }
 </script>
@@ -19,7 +63,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 img{
